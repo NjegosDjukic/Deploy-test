efff7d5693168bf9cd5cd894a6a6f03a9a14b505
NjegosDjukic <njegos.djukic@servalit.com>
2025-11-09 19:19:46 +0100

    Add welcome page

---
+++ WebApp/src/components/Welcome.tsx
@@ -0,0 +1,8 @@
+export const Welcome = () => {
+  return (
+    <div className="flex flex-col">
+      <span>Welcome to my app</span>
+      <span>This automaticaly deployed</span>
+    </div>
+  );
+};
